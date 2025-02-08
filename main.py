6fd7b320d443416242f9e861aca4d5c9196c4574
kirill <135870344+dotrey7@users.noreply.github.com>
2025-02-08 16:27:45 +0300

    совместил свою часть и часть Эвы

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import os
 import sqlite3
+from pygame.math import Vector2
+import random
 
 SCREEN = WIDTH, HEIGHT = 800, 800
 FPS = 60
@@ -14,6 +16,8 @@ pygame.init()
 INDIAN_SOUND = pygame.mixer.Sound('data/click-for-game-menu-131903.mp3')
 SLIDE_IN = pygame.mixer.Sound('data/click-button-app-147358.mp3')
 EXIT_LOGIN_EVENT_TYPE = 31
+cell_size = 40
+cell_number = 20
 
 
 def load_image(name, colorkey=None):
@@ -260,28 +264,150 @@ def play_view():
         pygame.display.flip()
 
 
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+        self.direction = Vector2(1, 0)
+        self.new_block = False
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            snake_block = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), snake_block)
+            # создаём пямоуголькин
+
+    def move_snake(self):
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True
+
+
+class Fruit:
+    def __init__(self):
+        self.randomise()
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        screen.blit(apple, fruit_rect)
+        # pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+
+    def randomise(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+
+class MAIN:
+    def __init__(self):
+        self.snake = Snake()
+        self.fruit = Fruit()
+        self.running = True
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+        self.check_fail()
+
+    def draw_elements(self):
+        self.draw_grass()
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            # print('snack')
+            # переместить фрукт в другую ячейку и создать новый блок в змее
+            self.fruit.randomise()
+            self.snake.add_block()
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+
+    def game_over(self):
+        self.running = False
+
+    def draw_grass(self):
+        grass_color = (167, 209, 61)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+
+
+SCREEN_UPDATE = pygame.USEREVENT
+
+image = ''
+apple = ''
+
+
 def level_one_loop():
+    global image, apple
     clicked = False
-    men = Menu()
     exit_button = Button(20, 20, 120, 45, "Exit")
     running = True
-    while running:
+    clock = pygame.time.Clock()
+    image = load_image('data/apple.png')
+    apple = pygame.transform.scale(image, (40, 40))
+    main_game = MAIN()
+    game_font = pygame.font.Font(None, 25)
+    pygame.time.set_timer(SCREEN_UPDATE, 150)
+    while main_game.running:
         pos = pygame.mouse.get_pos()
 
         if exit_button.is_clicked(pos):
             if clicked:
-                running = False
+                main_game.running = False
 
         clicked = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                main_game.running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 clicked = True
+            if event.type == SCREEN_UPDATE:
+                main_game.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    if main_game.snake.direction.y != 1:
+                        main_game.snake.direction = Vector2(0, -1)
+                if event.key == pygame.K_RIGHT:
+                    if main_game.snake.direction.x != -1:
+                        main_game.snake.direction = Vector2(1, 0)
+                if event.key == pygame.K_DOWN:
+                    if main_game.snake.direction.y != -1:
+                        main_game.snake.direction = Vector2(0, 1)
+                if event.key == pygame.K_LEFT:
+                    if main_game.snake.direction.x != 1:
+                        main_game.snake.direction = Vector2(-1, 0)
 
-        men.render(screen)
+        screen.fill((175, 215, 70))
+        main_game.draw_elements()
         exit_button.render(screen)
         pygame.display.flip()
+        clock.tick(60)
 
 
 def level_two_loop():
